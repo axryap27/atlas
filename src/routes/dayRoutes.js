b0e8417157aa7e5f924b2107fb2859238b6c57cd
aarya <aarya27@gmail.com>
2025-07-15 15:59:46 -0400

    workout template handling for routing

--- src/routes/dayRoutes.js
+++ src/routes/dayRoutes.js
@@ -5,10 +5,23 @@ const { getWorkoutDays, createWorkoutDay, getWorkoutDayById } = require('../cont
 // GET /api/days - Get all workout days
 router.get('/', getWorkoutDays);
 
+// GET /api/days/templates - Get only templates
+router.get('/templates', getTemplates);
+
 // POST /api/days - Create new workout day
 router.post('/', createWorkoutDay);
 
+// POST /api/days/templates - Create new template specifically
+router.post('/templates', createTemplate);
+
 // GET /api/days/:id - Get specific workout day
 router.get('/:id', getWorkoutDayById);
+
+// DELETE /api/days/templates/:id - Delete a template
+router.delete('/templates/:id', deleteTemplate);
+
+
+
+
 
 module.exports = router;
