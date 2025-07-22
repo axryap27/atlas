8d24411513fbe6cd002f90012fee7b85dad445c6
aarya <aarya27@gmail.com>
2025-07-21 23:26:18 -0400

    debugging server issues

--- WorkoutTracker/app/components/ui/TabBarBackground.ios.tsx
+++ WorkoutTracker/app/components/ui/TabBarBackground.ios.tsx
@@ -1,6 +1,6 @@
-import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
-import { BlurView } from 'expo-blur';
-import { StyleSheet } from 'react-native';
+import { useBottomTabBarHeight } from "@/node_modules/@react-navigation/bottom-tabs/lib/typescript/src";
+import { BlurView } from "expo-blur";
+import { StyleSheet } from "react-native";
 
 export default function BlurTabBarBackground() {
   return (
