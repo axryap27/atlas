9b577a474795ff5681da62ffb3617bde34d5d4ac
aarya <aarya27@gmail.com>
2025-08-21 23:26:36 -0400

    fonts downloaded from expo library:

--- Atlas/app/_layout.tsx
+++ Atlas/app/_layout.tsx
@@ -2,6 +2,8 @@
 import { Stack } from 'expo-router';
 import AuthProvider from './components/auth/AuthProvider';
 import { useFonts, Inter_400Regular, Inter_500Medium, Inter_600SemiBold, Inter_700Bold } from '@expo-google-fonts/inter';
+import { FunnelDisplay_400Regular, FunnelDisplay_500Medium, FunnelDisplay_600SemiBold, FunnelDisplay_700Bold } from '@expo-google-fonts/funnel-display';
+import { Outfit_400Regular, Outfit_500Medium, Outfit_600SemiBold } from '@expo-google-fonts/outfit';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 
@@ -14,6 +16,13 @@ export default function RootLayout() {
     Inter_500Medium,
     Inter_600SemiBold,
     Inter_700Bold,
+    FunnelDisplay_400Regular,
+    FunnelDisplay_500Medium,
+    FunnelDisplay_600SemiBold,
+    FunnelDisplay_700Bold,
+    Outfit_400Regular,
+    Outfit_500Medium,
+    Outfit_600SemiBold,
   });
 
   useEffect(() => {
